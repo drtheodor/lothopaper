5e718197d8026922cccee67b476f69715bf1faea
Theo <git@theo.is-a.dev>
2025-12-07 13:39:44 +0300

    add clap & zon config

--- src/config.zig
+++ src/config.zig
@@ -1,15 +1,37 @@
 const std = @import("std");
+const mem = std.mem;
+const fs = std.fs;
+const zon = std.zon.parse;
+
+const Self = @This();
 
 // TODO: improve errors
-pub fn readConfigString(allocator: std.mem.Allocator, name: []const u8) ![]u8 {
+pub fn readConfigString(allocator: mem.Allocator, name: []const u8) ![]u8 {
     const path = try getConfigPath(allocator, name);
     defer allocator.free(path);
 
-    return try std.fs.cwd().readFileAlloc(allocator, path, std.math.maxInt(usize));
+    return try fs.cwd().readFileAlloc(allocator, path, 8 * 1024 * 1024);
+}
+
+pub fn readConfig(allocator: std.mem.Allocator) Self {
+    const path = try getConfigPath(allocator, "config.zon");
+    defer allocator.free(path);
+
+    const diag: ?zon.Diagnostics = undefined;
+    const res = zon.fromSlice(Self, allocator, path, &diag, .{
+        .free_on_error = true,
+        .ignore_unknown_fields = false,
+    });
+
+    if (diag) |d| {
+        d.format(fs.File.stdout());
+    }
+
+    return res;
 }
 
 // Build ~/.config/lothopaper/config/<filename>
-fn getConfigPath(allocator: std.mem.Allocator, filename: []const u8) ![]u8 {
+fn getConfigPath(allocator: mem.Allocator, filename: []const u8) ![]u8 {
     // Get the environment variable "$HOME"
     const home = try std.process.getEnvVarOwned(allocator, "HOME");
     defer allocator.free(home);
@@ -21,5 +43,5 @@ fn getConfigPath(allocator: std.mem.Allocator, filename: []const u8) ![]u8 {
         filename,
     };
 
-    return try std.fs.path.join(allocator, &parts);
+    return try fs.path.join(allocator, &parts);
 }
